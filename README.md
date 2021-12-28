6ca5680131c3f591fc08e900655d6e7d0d956c36
a <a@a.com>
2021-12-28 14:55:05 -0700

    Add audio support in M4As

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ A GUI frontend for yt-dlp in PowerShell.
 
 ## How to run
 Just do the normal right click `Run With PowerShell`. The app might take a while to open on first launch, however.
+
+## How to save as audio
+In the location prompt, you can choose the type. There is a catch, however: The audio file is a `.m4a`.
+### What is an M4A?
+A `.m4a` file is like a `.mp3` file, but M4As are better in quality and bigger files. If you need to, you can use an online converter website (such as http://cloudconvert.com).
--- downloader.ps1
+++ downloader.ps1
@@ -16,18 +16,28 @@ if (Test-Path -Path "$ytdl" -PathType Leaf) {
 
 $link = $([Microsoft.VisualBasic.Interaction]::InputBox("YouTube link:", "Link"))
 $fdial = New-Object System.Windows.Forms.SaveFileDialog
-$fdial.Filter = "MP4 files (*.mp4)|*.mp4"
+$fdial.Filter = "MP4 video (*.mp4)|*.mp4|M4A audio (*.m4a)|*.m4a"
 $fdial.ShowDialog()
 $name = $fdial.FileName
+$ext1 = $name.Split(".")
+$ext = $ext1[$ext1.Count - 1]
 Write-Output @"
 Do
 MsgBox "downloading..."
 Loop
 "@ > $env:TEMP\downloading.vbs
 wscript $env:TEMP\downloading.vbs
-cmd /c "$ytdl -o video $link"
-cmd /c 'move video.* video'
-ffmpeg -i video -vcodec copy -c:a copy video.mp4
-cmd /c "move video.mp4 $name"
+if ($ext -eq "m4a") {
+    cmd /c "$ytdl -f `"m4a`" -o video $link"
+    cmd /c "move video $name"
+} elseif ($ext -eq "mp4") {
+    cmd /c "$ytdl -o video $link"
+    cmd /c 'move video.* video'
+    ffmpeg -i video -vcodec copy -c:a copy "video.mp4"
+    cmd /c "move video.mp4 $name"
+}
+$olderror = $ErrorActionPreference
+$ErrorActionPreference = 'silentlycontinue'
 Remove-Item video
+$ErrorActionPreference = $olderror
 taskkill /f /im wscript.exe
